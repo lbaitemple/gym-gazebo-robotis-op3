dafbce04f1a6d8f3d183d599390e0d433c37aa81
smilu97 <smilup2244@gmail.com>
2021-02-11 14:29:37 +0900

    Ignore "~/.bashrc" setup command

--- gym_gazebo/envs/installation/setup_melodic.bash
+++ gym_gazebo/envs/installation/setup_melodic.bash
@@ -30,8 +30,8 @@ vcs import < ../../gazebo_ros_melodic.repos
 cd ..
 touch catkin_ws/src/ecl_navigation/ecl_mobile_robot/CATKIN_IGNORE
 # catkin_make
-bash -c 'echo source `pwd`/devel/setup.bash >> ~/.bashrc'
-echo "## ROS workspace compiled ##"
+# bash -c 'echo source `pwd`/devel/setup.bash >> ~/.bashrc'
+# echo "## ROS workspace compiled ##"
 
 # add own models path to gazebo models path
 if [ -z "$GAZEBO_MODEL_PATH" ]; then
